34d949e5af9559c57eacc2c1b75b93c9ae41665c
chenwd <chenwd@chinawidth.cn>
2017-06-15 17:10:53 +0800

    Initial commit

---
+++ app/src/main/java/com/dong/PopupWindow/MyOnclickListener.java
@@ -0,0 +1,11 @@
+package com.dong.PopupWindow;
+
+import android.view.View;
+
+/**
+ * Created by MQ on 2017/3/13.
+ */
+
+public interface MyOnclickListener {
+     void onItemClick(View view, int position);
+}
